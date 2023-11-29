2a699418b44567c281f4c001e3553addc9257736
Paul <paul@duhard.com>
2023-11-29 23:46:40 +0100

    openprops clean

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -53,55 +53,11 @@
 
 <style>
 	@import "open-props/style";
-	@import "open-props/gray-hsl";
-
-	/* @font-face {
-		font-family: "Anton";
-		font-style: "normal";
-		font-weight: 100 900;
-		font-display: swap;
-		src: local(""), url("/anton.woff2") format("woff2");
-		unicode-range: U+20-7E, U+E4, U+E5, U+F6;
-	} */
-
-	:root {
-		--brand: var(--red-6);
-		--text-1: var(--gray-9);
-		--text-2: var(--gray-11);
-		--surface-1: var(--gray-3);
-		--surface-2: var(--indigo-2);
-		--surface-3: var(--gray-2);
-		--surface-4: var(--orange-4);
-		--surface-shadow: var(--gray-8-hsl);
-		--shadow-strength: 2%;
-	}
-
-	@media (prefers-color-scheme: dark) {
-		:root {
-			color-scheme: dark;
-
-			--brand: var(--orange-10);
-			--text-1: var(--gray-3);
-			--text-2: var(--gray-2);
-			--surface-1: var(--gray-9);
-			--surface-2: var(--gray-10);
-			--surface-3: var(--purple-12);
-			--surface-4: var(--violet-11);
-			--surface-shadow: var(--gray-11-hsl);
-			--shadow-strength: 50%;
-		}
-	}
-
-	/* Set fonts */
 	@font-face {
     font-family: "forma-djr-deck", sans-serif;
-    /* font-weight: 500; */
-    /* font-style: normal; */
 }
 	@font-face {
 		font-family: "latienne-pro", serif;
-		/* font-weight: 400; */
-		/* font-style: italic; */
 	}
 
 </style>

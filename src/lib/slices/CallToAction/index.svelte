d938fa51a1e6a6a08071e5f2a7940711528b67f4
Paul <paul@duhard.com>
2023-11-30 11:18:19 +0100

    more styling

--- src/lib/slices/CallToAction/index.svelte
+++ src/lib/slices/CallToAction/index.svelte
@@ -1,41 +1,25 @@
 <script>
 
-		import { PrismicRichText } from "@prismicio/svelte";
 		import { PrismicLink } from '@prismicio/svelte';
 
 		/** @type {import("@prismicio/client").Content.CallToActionSlice} */		
 
-	    export let links;
 		export let slice;
 
 </script>
 
 
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	<div class="cta mt-8 mb-24">
+	<div class="cta mt-8 mb-24 flex flex-col justify-center items-center gap-6 md:flex-row">
 			{#each slice.items as cta, index}
 			<PrismicLink field={cta.link_url}>{cta.link_label}</PrismicLink>
-			
 			{/each }
 		</div>
 	</section>
 
  <style>
-	.cta {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		gap: var(--size-8);
-	}
-
-	@media only screen and (min-width: 768px) {
-		.cta {
-			flex-direction: row;
-		}
-		}
-
 	:global(a) {
+		font-family: "forma-djr-deck, sans-serif";
 		color: var(--text-2);
         position: relative;
         text-decoration: none;
@@ -51,5 +35,4 @@
     border-top: 1px solid black;
     border-bottom: 1px solid black;
     }
-
  </style>
